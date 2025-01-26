1d2a2b59fb2d333d7378faa10d4486fca7d4b6b1
devfemibadmus <devfemibadmus@gmail.com>
2025-01-26 11:00:11 +0100

    🚧 sta 📝 tics 🔥

---
+++ statics/product.js
@@ -0,0 +1,22 @@
+cars = [
+    {
+        'name': 'SUPER CLEAN!!Foreign used GLK350 4matic 2012 model fully loaded',
+        'image': 'statics/cars/car.jpg',
+    },
+    {
+        'name': 'SUPER CLEAN!!Foreign used GLK350 4matic 2012 model fully loaded',
+        'image': 'statics/cars/car.jpg',
+    },
+    {
+        'name': 'SUPER CLEAN!!Foreign used GLK350 4matic 2012 model fully loaded',
+        'image': 'statics/cars/car.jpg',
+    },
+    {
+        'name': 'SUPER CLEAN!!Foreign used GLK350 4matic 2012 model fully loaded',
+        'image': 'statics/cars/car.jpg',
+    },
+    {
+        'name': 'SUPER CLEAN!!Foreign used GLK350 4matic 2012 model fully loaded',
+        'image': 'statics/cars/car.jpg',
+    },
+]
